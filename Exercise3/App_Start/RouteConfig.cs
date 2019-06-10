c4d9b15eb008b1a4949da9bf1959095c2413198e
root <root@DESKTOP-U49BLG5.localdomain>
2019-06-10 19:06:29 +0300

    added the HomeController and took care of some other stuff

--- Exercise3/App_Start/RouteConfig.cs
+++ Exercise3/App_Start/RouteConfig.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace Exercise3
+namespace REST_WEB
 {
     public class RouteConfig
     {
@@ -13,20 +9,32 @@ namespace Exercise3
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            // Route to save panel
-            routes.MapRoute("savePanel", "save/{ip}/{port}/{time}/{timeout}/{filePath}",
-            defaults: new { controller = "Home", action = "savePanel" });
 
-            // Route to display Panel
-            routes.MapRoute("displayPanel", "display/{ip}/{port}/{time}",
-            defaults: new { controller = "Home", action = "displayPanel", time = UrlParameter.Optional });
-
-            // Route to default panel
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "displayPanel",
+                url: "display/{ipOffset0}.{ipOffset1}.{ipOffset2}.{ipOffset3}/{port}/{time}",
+                 defaults: new { controller = "Home", action = "displayPanel", time = 0 }
+            );
+
+            routes.MapRoute(
+                name: "saveDisplay",
+                url: "save/{ip}/{port}/{second}/{time}/{name}",
+                defaults: new { controller = "Home", action = "saveDisplay" }
+            );
+
+            routes.MapRoute(
+                name: "displayFile",
+                url: "display/{name}/{interval}",
+                defaults: new { controller = "Home", action = "displayFile" }
+            );
+
+            routes.MapRoute(
+            name: "Default",
+            url: "{action}/{id}",
+            defaults: new { controller = "Home", action = "Def", id = UrlParameter.Optional }
             );
         }
+
     }
+
 }
--- Exercise3/Controllers/HomeController.cs
+++ Exercise3/Controllers/HomeController.cs
@@ -1,30 +1,99 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Text;
+using System.Xml;
 
 namespace Exercise3.Controllers
 {
-    public class HomeController : Controller
+    public class MainController : Controller
     {
-        public ActionResult Index()
+        private ClientModel ClientModel = ClientModel.Instance;
+        private string requestUserName = null;
+
+        [HttpGet]
+        public ActionResult displayPanel(string ipOffest0, string ipOffest1, string ipOffest2, 
+                                    string ipOffest3, int port, int timeAsInt)
         {
+            string ip = ipOffest0 + "." + ipOffest1 + "." + ipOffest2 + "." + ipOffest3;
+            ClientModel.Open(ip, port);
+
+            if (ClientModel.IsConnected())
+                Session["time"] = timeAsInt;
+
             return View();
         }
 
-        public ActionResult About()
+        [HttpGet]
+        public ActionResult saveDisplay(string ip, int port, int timeAsInt, int fromStartSeconds, string clientName)
         {
-            ViewBag.Message = "Your application description page.";
+            ClientModel.Open(ip, port);
+            ClientModel.Name = clientName;
+
+            Session["time"] = fromStartSeconds;
+            Session["timoutSave"] = timeAsInt;
 
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpGet]
+        public ActionResult displayFile(string username, int interval)
         {
-            ViewBag.Message = "Your contact page.";
+            Session["interval"] = interval;
+            Session["name"] = username;
+
+            requestUserName = username;
 
             return View();
+        }
+
+        [HttpPost]
+        public string displayToUser()
+        {
+            string addString = @"\" + Session["name"] + ".xml";
+
+            string filename = 
+                AppDomain.CurrentDomain.BaseDirectory + addString;
+
+            return DataBaseHandler.Instance.ReadData(filename);
+        }
+
+        [HttpPost]
+        public string GetLongLat()
+        {
+            return ToXml(ClientModel.Location);
+        }
+
+
+        public string ToXml(LocationModel loc)
+        {
+            // Initialize stringbuilder
+            StringBuilder stringBuilder = new StringBuilder();
+
+            // create an xmlWriterSettings
+            XmlWriterSettings st = new XmlWriterSettings();
+
+            // create an XML writer
+            XmlWriter writer = XmlWriter.Create(stringBuilder, st);
+
+            // start writing
+            writer.WriteStartDocument();
+            writer.WriteStartElement("LocationModel"); // LocationModel
+
+            // write
+            loc.ToXml(writer);
+
+            // long, lat
+            writer.WriteElementString("Long", loc.Lon);
+            writer.WriteElementString("Lat", loc.Lat);
+
+            // write
+            writer.WriteEndElement();
+
+            // end
+            writer.WriteEndDocument();
+            writer.Flush(); // empty writer
+
+            return stringBuilder.ToString();
         }
     }
 }
